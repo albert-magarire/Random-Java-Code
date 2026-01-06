4bd7acceb3b91f0850702b17215f1bb73faecf94
Betserai Albert Magarire <albertmagarire@gmail.com>
2026-01-06 08:58:00 -0500

    Constructors

--- src/Game.java
+++ src/Game.java
@@ -1,8 +1,11 @@
 public class Game{
     public static void main(String[] args) {
-        Hero hero = new Hero();
-        hero.name = "Sysco";
-        hero.powerLevel = 3;
-        hero.announce();
+//        Hero hero = new Hero();
+//        hero.name = "Sysco";
+//        hero.powerLevel = 3;
+//        hero.announce();
+
+        Hero zelda = new Hero("ZELDA", 100);
+        zelda.announce();
     }
 }
--- src/Hero.java
+++ src/Hero.java
@@ -2,6 +2,11 @@ public class Hero {
     String name;
     int powerLevel;
 
+    public Hero(String name, int powerLevel){
+        this.name = name;
+        this.powerLevel = powerLevel;
+    }
+
     void announce(){
         System.out.println("I am " + name + " and I have a power level of " + powerLevel);
     }
