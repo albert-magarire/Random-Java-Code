e3b19332c6d5d209b854ff98c33d1ca112c5f42b
Betserai Albert Magarire <albertmagarire@gmail.com>
2026-01-06 09:07:26 -0500

    Constructor chaining

--- src/Hero.java
+++ src/Hero.java
@@ -7,6 +7,19 @@ public class Hero {
         this.powerLevel = powerLevel;
     }
 
+    public Hero(String name){
+        this.name = name;
+        this.powerLevel = 1;
+
+        //to avoid repetition, we use constructor chaining in which the default defined constructor is called
+        // this(name, 1)
+    }
+
+    public Hero(int powerLevel){
+        //this.powerLevel = powerLevel;
+        this("Albert", 25);
+    }
+
     void announce(){
         System.out.println("I am " + name + " and I have a power level of " + powerLevel);
     }
